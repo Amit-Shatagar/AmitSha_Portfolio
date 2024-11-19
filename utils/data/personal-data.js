94d0008b093b169cf1f76cf9754c46562647947a
Amitsha <135249871+Amit-Shatagar@users.noreply.github.com>
2024-11-19 13:18:18 +0000

    update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,5 +1,5 @@
 export const personalData = {
-  name: "AMIT SHATAGAR",
+  name: "AMIT SHETTY",
   profile: '/profile.png',
   designation: " FullStack Developer",
   description: "My name is AMIT SHATAGAR. I am a Fresher and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
